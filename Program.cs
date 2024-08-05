c57bd6348a6cb719510ead6777082db3fcb46987
Raider123 <47940054+Raider123@users.noreply.github.com>
2024-08-05 21:27:54 +0200

    First Test Console Dead Application

--- Program.cs
+++ Program.cs
@@ -1,54 +1,19 @@
-using NoGravEx_Entwurf2;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
+using Waveplus_Daq_Example;
 
 namespace Waveplus.DaqExample
 {
-    static class Program
+    class Program
     {
-        private static NoGravEx_V2 ngev2;
-
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-                
-        static void Main()
-        {            
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            try
-            {
-                ngev2 = new NoGravEx_V2();
-                Application.Run(ngev2);
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-            finally
-            {
-                if (ngev2 != null) ngev2.Dispose();
-                Application.Exit();
-                Environment.Exit(0);
-            }
-        }
-
-        /// <summary>
-        /// Assigns a different color to the progressbar used in the GameForm
-        /// </summary>
-        /// <param name="hWnd"></param>
-        /// <param name="Msg"></param>
-        /// <param name="w"></param>
-        /// <param name="l"></param>
-        /// <returns></returns>
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
-        public static void SetState(this ProgressBar pBar, int state)
+        static void Main(string[] args)
         {
-            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
+            Sender sender = new Sender();
+            sender.Run();
         }
     }
+
 }
+
---
+++ Sender.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using LSL;
+using Waveplus.DaqExample;
+
+namespace Waveplus_Daq_Example
+{
+    internal class Sender
+    {
+        // The EMG example program
+        private WaveplusDaqExampleForm wdaq;
+
+        // LSL related fields
+        private StreamOutlet outlet;
+
+        public Sender()
+        {
+            wdaq = new WaveplusDaqExampleForm();
+            InitializeLSL();
+        }
+
+        private void InitializeLSL()
+        {
+            // Erstellen von StreamInfo für den Stream
+            StreamInfo streamInfo = new StreamInfo("ExampleStream", "EMG", 1, 100, channel_format_t.cf_double64, "myuid34234");
+
+            // Erstellen eines StreamOutlet
+            outlet = new StreamOutlet(streamInfo);
+        }
+
+        private void SendEMGData(double current_emg_samples)
+        {
+            // Erstellen eines Arrays für den zu sendenden Wert
+            double[] sample = new double[1] { current_emg_samples };
+
+            // Senden der EMG-Daten
+            outlet.push_sample(sample);
+        }
+
+        private void Update()
+        {
+            double current_emg_samples = wdaq.current_emg_sample;
+            Debug.Write(current_emg_samples.ToString());
+            SendEMGData(current_emg_samples);
+        }
+
+        public void Run()
+        {
+            // Starte die Endlosschleife
+            while (true)
+            {
+                Update();
+
+                // Kurze Pause, um CPU-Überlastung zu vermeiden (z.B. 10ms)
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
